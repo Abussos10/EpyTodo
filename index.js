8a29385a28eb046ca8a425455dd90fecefb39957
ballandlucien <lucien.balland@epitech.eu>
2023-05-21 08:05:05 +0200

    [BEAUTIFY] and comentary deletion

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express       =       require("express");
-const db            =       require("./routes/db-config");
-const app           =       express();
-const cookie        =       require("cookie-parser");
-const PORT          =       process.env.PORT || 5000
+const express = require("express");
+const db = require("./routes/db-config");
+const app = express();
+const cookie = require("cookie-parser");
+const PORT = process.env.PORT || 5000
 
 app.use("/js", express.static(__dirname + "/public/js"))
 app.use("/css", express.static(__dirname + "/public/css"))
@@ -11,10 +11,6 @@ app.set("views", "./views");
 app.use(cookie());
 app.use(express.json());
 
-
-//db.connect((err) => {
-//    if (err) throw err;
-//})
 
 app.use("/", require("./routes/pages"));
 app.use("/api", require("./controllers/auth"));
