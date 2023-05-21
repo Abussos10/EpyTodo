201348f19acfa8850b1351d6b5d319298db6480c
ballandlucien <lucien.balland@epitech.eu>
2023-05-21 08:03:56 +0200

    [BEAUTIFY] and comentary deletion

--- controllers/user.js
+++ controllers/user.js
@@ -1,29 +1,17 @@
 const db = require("../routes/db-config");
+const jwt = require("jsonwebtoken");
 
-const getUser = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    // Fetch user information from the database
-    const user = db.query("SELECT * FROM users WHERE id = ?", [userId]);
-
-    if (user.length === 0) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    const userInfo = {
-        id: user[0].id,
-        email: user[0].email,
-        password: user[0].password,
-        created_at: user[0].created_at,
-        firstname: user[0].firstname,
-        name: user[0].name,
-    };
-
-    res.json(userInfo);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+const getUserData = (userId, callback) => {
+    const sql = 'SELECT * FROM users WHERE id = ?';
+    db.query(sql, [userId], (err, result) => {
+        if (err) {
+            return callback(err);
+        }
+        if (!result[0]) {
+            return callback("User not found");
+        }
+        callback(null, result[0]);
+    });
 };
 
-module.exports = getUser;  
+module.exports = getUserData;
