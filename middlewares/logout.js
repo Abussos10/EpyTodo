e13897a17028638622da72046163f5693c25d6e2
ballandlucien <lucien.balland@epitech.eu>
2023-05-21 22:15:25 +0200

    [FINAL] push at root for backend version only

--- middlewares/logout.js
+++
@@ -1,9 +0,0 @@
-const logout_handling = async (req, res) => {
-    res.clearCookie("access_token");
-    res.json({
-        status: "success",
-        message: "Logout successful"
-    });
-};
-
-module.exports = logout_handling;
