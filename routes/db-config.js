f27cef4c7798e88331bd7023ca3f3b5a42a171ec
ballandlucien <lucien.balland@epitech.eu>
2023-05-21 08:13:03 +0200

    [BEAUTIFY] and console.log deletion

--- routes/db-config.js
+++ routes/db-config.js
@@ -2,18 +2,12 @@ const sql       =       require("mysql2");
 const dotenv    =       require("dotenv");
 dotenv.config();
 
-// some useful debug console log
-console.log(process.env.DATABASE_HOST);
-console.log(process.env.DATABASE_USER);
-console.log(process.env.DATABASE_PASSWORD);
-console.log(process.env.DATABASE);
-
 // db connection
 const db = sql.createConnection({
-    host:       process.env.DATABASE_HOST,
-    user:       process.env.DATABASE_USER,
-    password:   process.env.DATABASE_PASSWORD,
-    database:   process.env.DATABASE
+    host:       process.env.MYSQL_HOST,
+    user:       process.env.MYSQL_USER,
+    password:   process.env.MYSQL_ROOT_PASSWORD,
+    database:   process.env.MYSQL_DATABASE
 });
 
 // check if connection succeed
@@ -23,6 +17,6 @@ db.connect((err) => {
       return;
     }
     console.log("Connected to the database!");
-  });
+});
   
-  module.exports = db;
+module.exports = db;
